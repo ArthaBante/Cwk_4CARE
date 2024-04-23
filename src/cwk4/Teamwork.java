65173b0a4dffa57fd7358621e86aae143791a9fb
Artha Bante <arthabante0@gmail.com>
2024-04-23 21:35:46 +0100

    Initial commit

---
+++ src/cwk4/Teamwork.java
@@ -0,0 +1,54 @@
+package cwk4; 
+
+
+/**
+ * Details of your team
+ * 
+ * @author (Artha Bante)
+ * @version (14/04/2024)
+ */
+public class Teamwork
+{
+    private String[] details = new String[12];
+    
+    public Teamwork()
+    {   // in each line replace the contents of the String 
+        // with the details of your team member
+        // Please list the member details alphabetically by surname 
+        // i.e. the surname of member1 should come alphabetically 
+        // before the surname of member 2...etc
+        details[0] = "13";
+        
+        details[1] = "Bante";
+        details[2] = "Artha";
+        details[3] = "21085597";
+
+        details[4] = "Gohil";
+        details[5] = "Denil";
+        details[6] = "SRN of member2";
+
+        details[7] = "Verma";
+        details[8] = "Lakshay";
+        details[9] = "SRN of member3";
+
+
+        details[10] = "NA";
+        details[11] = "NA";
+        details[12] = "NA";
+
+    }
+    
+    public String[] getTeamDetails()
+    {
+        return details;
+    }
+    
+    public void displayDetails()
+    {
+        for(String temp:details)
+        {
+            System.out.println(temp.toString());
+        }
+    }
+}
+        
